ab18642c84398d5cebc47c881f3abe71d9382e81
JX Wang <wjianxu@gmail.com>
2026-05-04 18:25:38 +0800

    feat: 更新 open_vscode.sh 脚本为 zsh，修正工作区路径处理逻辑

--- VSCode-Recent-Workspaces/src/open_vscode.sh
+++ VSCode-Recent-Workspaces/src/open_vscode.sh
@@ -1,12 +1,13 @@
-#!/bin/bash
+#!/bin/zsh
 
 arg="$1"
 
-path="${arg%%||*}"
+workspace_path="${arg%%||*}"
 profile="${arg#*||}"
 
-if [ "$profile" = "Default" ]; then
-    code "$path"
-else
-    code --profile "$profile" "$path"
-fi
+# echo "Workspace Path: $workspace_path"
+# echo "Profile: $profile"
+
+# 以登录 shell 方式启动，以便能够载入 .zshrc 中的环境变量
+# 使用 -c 的特性：在命令字符串内使用 $1, $2 等位置参数，然后在 -c 后面按顺序提供实际的值。
+zsh -l -c 'code --profile "$1" "$2"' _ "$profile" "$workspace_path"
